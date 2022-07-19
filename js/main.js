11be232d77f503ca0442fd5e84a33db34b73253e
Leo Vásquez Vólquez <vasquezvolquezfelixleonel@gmail.com>
2022-07-19 12:02:10 -0400

    Generando iFrame al clickear imagen en sección What makes the Barkin-Gilman Group different

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@
 const hamburguer = document.getElementById("hamburguer");
 const listLinks = document.getElementById("listLinks");
 
-hamburguer.addEventListener('click', function () {
-    listLinks.classList.toggle("navBar__listLinks--active");
+/* EVENTOS */
+/* Expand or collapse the nav bar */
+hamburguer.addEventListener('click', () => listLinks.classList.toggle("navBar__listLinks--active") );
+
+/* Generate iFrame when the picture be clicked */
+videoContainer.addEventListener('click', function() {
+    videoContainer.innerHTML = ('<div class="whatMakesDifferent__img"><iframe width="560" height="315" src="https://www.youtube.com/embed/OA5v-bK67jg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>') ;  
 });
-
-
 
